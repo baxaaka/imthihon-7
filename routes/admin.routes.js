b8423b805e730b3b457dbb35b3a1579baea1ad94
baxaaka <jockuhacker@gmail.com>
2023-03-17 18:30:10 +0500

    tugadi

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -38,80 +38,18 @@ const Io = require("../utils/Io");
 const Valid = require("../models/validation");
 const User = require("../models/user.js");
 const Book = require("../models/book.js");
+const History = require("../models/history.js")
+
 const { json, text, urlencoded } = require("express");
 
 // JSONLARNI CHAQIRIB YANGI NEW  OCHAMIZ
 const Users = new Io("./db/users.json");
 const Valids = new Io("./db/validation.json");
 const Books = new Io("./db/books.json");
-const imageUpload = new Io("./db/imageUpload.json");
+const Historys = require("../db/history.json")
 
-//ADMIN GET METHOD
 
-// adminRoutes.get("/", async (req, res) => {
-//   try {
-//     const users = await Users.read();
 
-//     const admins = users.filter((user) => {
-//       if (user.isAdmin) {
-//         return user;
-//       }
-//     });
-
-//     res.status(200).json({ admins });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// ADMIN POST METHOD
-
-// adminRoutes.post("/admin/add", async (req, res) => {
-//   const { name, money, age, email } = req.body;
-
-//   const users = await Users.read();
-
-//   const id = (users[users.lenght - 1]?.id || 0) + 1;
-
-//   const newUser = new User(id, name, true, money, age, email);
-
-//   const allUsers = users.lenght ? [...users, newUser] : [newUser];
-
-//   Users.write(allUsers);
-
-//   console.log(allUsers);
-
-//   res.status(200).json({ message: "ok" });
-// });
-
-// Validation
-// adminRoutes.post("/validation", async (req, res) => {
-//   //# VALIDATION
-
-//   const { name, age, email } = req.body;
-
-//   const scheme = Joi.object({
-//     name: Joi.string().min(3).max(15).required(),
-//     // age : Joi.number().min(7).max(99).required(),
-//     email: Joi.string().email().required(),
-//   });
-
-//   const { error } = scheme.validate({ name, email });
-
-//   if (error) return res.status(400).json({ message: error.message });
-
-//   res.status(200).json({ name, email });
-
-//   const val = await Valids.read();
-
-//   const idV = (val[val.lenght - 1]?.id || 0) + 1;
-
-//   const newValid = new Valid(idV, name, age, email);
-
-//   const allValids = val.lenght ? [...val, newValid] : [newValid];
-
-//   Valids.write(allValids);
-// });
 
 adminRoutes.post("/register", async (req, res) => {
   try {
@@ -156,17 +94,31 @@ adminRoutes.post("/login", async (req, res) => {
     }
 
     const verified = await bcrypt.compare(password, user.password);
+  
     if (!verified) {
       return res.redirect("/register");
-      // console.log("bad login");
+    
     } else {
-      return res.redirect("/home");
-      // console.log("nice login");
+      return res.redirect("/home")
+
+    
+       
+ 
+       
+
+
     }
+    
+  
   } catch (error) {
     console.log(error.message);
   }
 });
+
+
+
+
+
 
 // ----blogggg------------------------
 
@@ -189,9 +141,22 @@ adminRoutes.get("/home", async (req, res) => {
 //blog  post
 adminRoutes.post("/home", async (req, res) => {
 
+    // # FILE-UPLOAD
+
+    const file = req.files.file;
+
+    //# UUID
+  
+    const format = file.mimetype.split("/")[1];
+  
+    const imageName = `${uuid()}.${format}`;
+    const path = `${process.cwd()}/upload/${imageName}`;
+  
+    // # mv - bu file ni upload qilip beradi
+    file.mv(path);
   
   
-  const {name, text  } = req.body;
+  const {name, text } = req.body;
 
 
 
@@ -200,23 +165,8 @@ adminRoutes.post("/home", async (req, res) => {
   const id = (book[book.length - 1]?.id || 0) + 1;
 
 
-  console.log(allUsers);
+ 
 
-  res.status(200).json({ message: "ok" });
-
-  // # FILE-UPLOAD
-
-  const file = req.files.file;
-
-  //# UUID
-
-  const format = file.mimetype.split("/")[1];
-
-  const imageName = `${uuid()}.${format}`;
-  const path = `${process.cwd()}/upload/${imageName}`;
-
-  // # mv - bu file ni upload qilip beradi
-  file.mv(path);
 
   // const imageUp = await imageUpload.read()
 
@@ -224,13 +174,15 @@ adminRoutes.post("/home", async (req, res) => {
 
   //  imageName.push(allUsers)
 
-  console.log(path);
 
   const newUser = new Book(id, name, text  , path );
 
   const allUsers = book.length ? [...book, newUser ] : [newUser];
 
   Books.write(allUsers);
+
+
+  console.log(path);
 
   res.status(200).json({ message: "ok keldi" });
 });
